caca92f7d160bb0fa297070294474bd98af0e4db
Sachin <sachinvidanapathirana@gmail.com>
2025-05-06 04:04:45 +0530

    Day 16✏️

--- controllers/productController.js
+++ controllers/productController.js
@@ -43,6 +43,21 @@ export function getProducts(req,res){
     )
 }
 
+export async function getProductById(req,res){
+    const productId = req.params.id
+    const product = await Product.findOne({productId : productId}) 
+    if(product == null){
+        res.status(403).json({
+            message : "Product not found"
+        })
+        return
+    }
+    res.json({
+        product : product
+    })
+     
+}
+
 export function deleteProduct(req,res){
     if(req.user == null){
         res.json({
