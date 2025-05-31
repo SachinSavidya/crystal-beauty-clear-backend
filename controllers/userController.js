2ac80410e0447abe5beaa677243e5898af6f4bf7
Sachin <sachinvidanapathirana@gmail.com>
2025-05-31 09:05:37 +0530

    Day 20 ✏️

--- controllers/userController.js
+++ controllers/userController.js
@@ -164,3 +164,16 @@ export async function googleLogin(req, res) {
         })
     }
 }
+
+export function getCurrentUser(req,res){
+    if(req.user == null){
+        res.status(403).json({
+            message : "Please login to get user deatils"
+        });
+        return;
+    }
+    res.json({
+        user : req.user
+    })
+
+}
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { googleLogin, saveUser, userlogin } from "../controllers/userController.js";
+import { getCurrentUser, googleLogin, saveUser, userlogin } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", saveUser);
 userRouter.post("/login", userlogin);
 userRouter.post("/google", googleLogin);
+userRouter.get("/current", getCurrentUser);
 
 export default userRouter;
