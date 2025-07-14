612762e74a699c6b3bc957155275194427604f39
kulunu sathmika <kulunusathmika69@gmail.com>
2025-07-14 21:39:49 +0530

    fix the production frontend url issue

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import userRouter from './routes/userRouter.js';
-import productRouter from './routes/productRouter.js';
-import verifyJWT from './middleware/auth.js';
-import orderRouter from './routes/orderRouter.js';
-import dotenv from 'dotenv';
-import cors from 'cors';
-dotenv.config()
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRouter.js";
+import productRouter from "./routes/productRouter.js";
+import verifyJWT from "./middleware/auth.js";
+import orderRouter from "./routes/orderRouter.js";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
 
 const app = express();
-app.use(cors({
+app.use(
+  cors({
     origin: [
-        "http://localhost:5173",                // local dev
-        "https://crystal-beauty-clear-kta1lqtd9-sachins-projects-a17017c3.vercel.app/"      // production frontend
+      "http://localhost:5173", // local dev
+      "https://crystal-beauty-clear.vercel.app/", // production frontend
     ],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
-mongoose.connect(process.env.MONGO_URL).then(
-    ()=>{
-        console.log("Connected to the data base");
-    }
-).catch(
-    ()=>{
-        console.log("Connection failed");
-    }
-)
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to the data base");
+  })
+  .catch(() => {
+    console.log("Connection failed");
+  });
 
 app.use(bodyParser.json());
 app.use(verifyJWT);
@@ -35,9 +36,6 @@ app.use("/api/product", productRouter);
 app.use("/api/user", userRouter);
 app.use("/api/order", orderRouter);
 
-
-app.listen(5000,
-    ()=>{
-        console.log("Server is running on port 5000");
-    }
-);
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
